693d3f0c70c8e54c8ed5ccd58e33f03968ca0363
Kyohei Oka <0622okakyo@gmail.com>
2018-07-27 20:08:26 +0900

    prottype

---
+++ start.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import Animation
+from matplotlib.widgets import Widget
+
+def fk(L,th):
+    l1,l2,l3=L
+    th1,th2,th3=th
+    
+
+    return
+
+def main():
+    return
