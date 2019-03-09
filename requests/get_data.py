5b1f797d37e6ef47198583a84af5b6a64b52a42e
Kyohei Oka <0622okakyo@gmail.com>
2019-03-09 14:34:06 +0900

    '練習中'

--- requests/get_data.py
+++ requests/get_data.py
@@ -1,11 +1,9 @@
 import sys,requests,json
+from bs4 import BeautifulSoup
 
-BaseUrl='http://api.e-stat.go.jp/rest/2.1/app/getStatsData?appId=b09e33724c5a0b1568a78b2598db83e8dd91bd94&lang=J&statsDataId=0003191320&metaGetFlg=Y&cntGetFlg=N&sectionHeaderFlg=1'
+BaseUrl='https://www.travel-advisory.info/api?countrycode=IN'
 
-def GetData(url):
-    result=requests.get(url)
-    return result.text
-
-if __name__=='__main__':
-    data=GetData(BaseUrl)
-    print(data)
+url=requests.get(BaseUrl)
+soup=BeautifulSoup(url.content,"lxml")
+data=soup.find("p")
+print(data)
