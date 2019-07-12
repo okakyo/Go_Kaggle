da2986b1bfb2e17fc5159735155e158f8bc65399
okakyo <0622okakyo@gmail.com>
2019-07-12 14:47:17 +0900

    VideoDL用のシステム完了

--- internet/getData.py
+++ internet/getData.py
@@ -1,6 +1,8 @@
 import requests
 from tqdm import tqdm
-video_url="http://51.255.94.107/Hitou Meguri Kakure Yu Mao Hen 1 Raw.mp4"
+
+
+video_url=input("videoのURLを入力してください。>>")
 
 if requests.head(video_url).status_code == 302 :
     video_url = requests.head(video_url).headers["Location"]
@@ -9,7 +11,7 @@ file_size = int(requests.head(video_url).headers["content-length"])
 response = requests.get(video_url,stream=True)
 
 
-with open("check3.mp4","wb") as file:
+with open("UnderTale.mp4","wb") as file:
     pbar = tqdm(total=file_size, unit="B", unit_scale=True)
     for chunk in response.iter_content(chunk_size=1024):
         ff= file.write(chunk)
--- internet/tcp.py
+++ internet/tcp.py
@@ -1,12 +1,5 @@
-import socket
-
-target_host="www.google.com"
-target_port=80
-
-client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-client.connect((target_host,target_port))
-
-client.send(b"GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
-response=client.recv(4096)
-print(response)
+import os
+print(os.path.abspath(__file__))
+print(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
